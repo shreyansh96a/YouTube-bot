f555edca3f6f22dd0c9791e41ab6f1c97d66ffb5
shreyansh96a <shreyanshmaurya122009@gmail.com>
2026-08-03 16:19:59 +0530

    Initialize handlers package with imports

---
+++ handlers/init.py
@@ -0,0 +1,6 @@
+"""
+Handlers package initialization
+"""
+from . import start, menu, script, title, description, tags, thumbnail
+from . import image_prompt, video_prompt, caption, hashtag, translate
+from . import history, favorites, referral, premium, profile, settings
